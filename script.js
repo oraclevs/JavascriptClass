d430ca2a857a453a4f51e9993b79c92aa0147baf
oraclevs <denton.mike888@gmail.com>
2023-08-15 13:50:24 +0100

    Introduction to JavaScript

---
+++ script.js
@@ -0,0 +1,82 @@
+
+/*  values and variables */ 
+console.log("hello world");
+
+let firstname = "Charles";
+
+console.log(firstname);
+
+/* ****************  Variable Naming Conventions *************** */
+let Charles4 = "Charles"
+let _Charles = "AM hungry"
+let $Charles = "Rice"
+console.log(Charles4, _Charles, $Charles);
+
+
+// **************************  Data Types  ************************
+/*
+1. string
+2. number
+3. boolean
+4.bigint
+5.undefined
+6.null
+7.symbol
+8.Object
+*/
+
+// Boolean -> True or False Vales 
+var isjavascriptEasy = true;
+var IslifeHard = false;
+console.log(typeof isjavascriptEasy);
+
+// Number
+let xcharles = 30;
+let ycharles = 29.8
+console.log(typeof xcharles);
+console.log(typeof ycharles);
+
+// strings 
+let Message = "Come to France"
+console.log(typeof Message);
+
+
+let Donald = "24" + 2;
+console.log(typeof Donald, Donald);
+
+// Undefined
+let year;
+console.log(year, typeof year);
+// An Empty Value has nothing to do with Undefined
+let car = '';
+console.log(typeof car, car);
+
+// When Adding a number and a srting, Javascript will treat the Number as a string.
+let vechicle = 16 + 4 + "volvo"
+console.log(vechicle, typeof vechicle);
+
+/* Null   
+In JavaScript Null is Nothing. it is supposed to be something that doesn't exist.
+The data Type of NUll is Object.
+*/
+let Money = null;
+console.log(Money, typeof Money);
+
+/* 
+* Difference between undefined and  null
+* Type of undefined is undefined
+* Type of null is object
+*/
+
+// similarites 
+console.log(null === undefined);
+
+
+// git init
+// git commit 
+
+
+
+
+
+
